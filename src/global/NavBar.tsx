17fb0b400204b1707ab049e043bcd2904b22a337
Trevor <trevorchew.kh@gmail.com>
2024-10-28 15:06:45 +0800

    Update NavBar.tsx
    Removed light / dark modes.
    Resized columns.

--- src/global/NavBar.tsx
+++ src/global/NavBar.tsx
@@ -1,32 +1,13 @@
-import logo_light from "../../src/assets/logo-light-mode.png";
-import logo_dark from "../../src/assets/logo-dark-mode.png";
-import search_icon_light from "../../src/assets/search-icon-light-mode.png";
-import search_icon_dark from "../../src/assets/search-icon-dark-mode.png";
-import toggle_light from "../../src/assets/toggle-light-mode.png";
-import toggle_dark from "../../src/assets/toggle-dark-mode.png";
+import logo from "../../src/assets/logo.png";
+import search_icon from "../../src/assets/search-icon.png";
 
-interface Props {
-  theme: "dark" | "light";
-  setTheme: (item: string) => void;
-}
-
-const NavBar = ({ theme, setTheme }: Props) => {
-  const toggle_mode = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
-
+const NavBar = () => {
   return (
     <>
-      <nav
-        className={
-          theme == "light"
-            ? "navbar navbar-expand-lg bg-body-tertiary"
-            : "navbar navbar-expand-lg bg-body-tertiary bg-dark navbar-dark"
-        }
-      >
+      <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary navbar-accent">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img src={theme == "light" ? logo_light : logo_dark} alt="logo" />
+          <a className="navbar-brand me-0 p-3" href="/">
+            <img src={logo} alt="logo" />
           </a>
 
           <button
@@ -42,40 +23,35 @@ const NavBar = ({ theme, setTheme }: Props) => {
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+            <ul className="navbar-nav me-auto p-2">
+              <li className="nav-item p-3">
                 <a className="nav-link" href="/films">
                   FILMS
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item p-3">
                 <a className="nav-link" href="/genres">
                   GENRES
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item p-3">
                 <a className="nav-link" href="/stars">
                   STARS
                 </a>
               </li>
             </ul>
 
-            <div className="search-bar">
-              <input type="text" placeholder="Search..." />
-              <img
-                src={theme == "light" ? search_icon_light : search_icon_dark}
-                alt=""
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
               />
-            </div>
-
-            <div className="toggle-light-dark">
-              <img
-                src={theme == "light" ? toggle_dark : toggle_light}
-                alt=""
-                className="toggle-icon"
-                onClick={() => toggle_mode()}
-              />
-            </div>
+              <button className="btn btn-outline-light" type="submit">
+                <img className="search-icon" src={search_icon} alt="" />
+              </button>
+            </form>
           </div>
         </div>
       </nav>
