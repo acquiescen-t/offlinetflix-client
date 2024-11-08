e4b1432da9ed5de225ae21a0e93b87abd6576c86
Trevor <trevorchew.kh@gmail.com>
2024-11-08 17:01:52 +0800

    Update Genre.tsx
    Genres are now fetched from database

--- src/model/Genre.tsx
+++ src/model/Genre.tsx
@@ -1,35 +1,11 @@
-import psychhorror from "../assets/genres/psychological-horror.jpg";
-import psychthriller from "../assets/genres/psychological-thriller.jpg";
-import drama from "../assets/genres/drama.jpg";
-import fantasy from "../assets/genres/fantasy.jpg";
-import horror from "../assets/genres/horror.jpg";
-import mystery from "../assets/genres/mystery.jpg";
-import scifi from "../assets/genres/science-fiction.jpg";
-import thriller from "../assets/genres/thriller.jpg";
-import comedy from "../assets/genres/comedy.jpg";
-
 export class Genre {
+  id: string;
   name: string;
   imageUrl: string;
 
-  constructor(name: string, imageUrl: string) {
+  constructor(id: string, name: string, imageUrl: string) {
+    this.id = id;
     this.name = name;
     this.imageUrl = imageUrl;
-  }
-
-  public static GenerateGenres(): Genre[] {
-    var genres = [
-      new Genre("Psychological Horror", psychhorror),
-      new Genre("Psychological Thriller", psychthriller),
-      new Genre("Drama", drama),
-      new Genre("Fantasy", fantasy),
-      new Genre("Horror", horror),
-      new Genre("Mystery", mystery),
-      new Genre("Science Fiction", scifi),
-      new Genre("Thriller", thriller),
-      new Genre("Comedy", comedy),
-    ];
-
-    return genres;
   }
 }
