d2029583c8857bab1e966594dc3270f1ad91251e
Trevor <trevorchew.kh@gmail.com>
2024-11-18 13:43:38 +0800

    Create GenreFilms.tsx
    Added page to list all films of a certain genre.

---
+++ src/pages/GenreFilms.tsx
@@ -0,0 +1,40 @@
+import Card from "../components/Card";
+import api from "../api";
+import { useState, useEffect } from "react";
+import { Film } from "../model/Film";
+import { useParams } from "react-router-dom";
+
+const GenreFilms = () => {
+  let { genreName } = useParams();
+
+  const [films, setFilms] = useState<Film[]>([]);
+  useEffect(() => {
+    api
+      .get("/films/search/genre-name/".concat(genreName!))
+      .then((response) => {
+        setFilms(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="container pt-5">
+        <div className="page-header text-uppercase fs-5">
+          <h3>{genreName?.concat(" FILMS")}</h3>
+        </div>
+        <div className="row g-5">
+          {films.map((item) => (
+            <div key={item.id} className="col-3">
+              <Card film={item}></Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GenreFilms;
