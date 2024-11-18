b30c746daaff33d7500d663cf51dcf77b71c23a3
Trevor <trevorchew.kh@gmail.com>
2024-11-18 13:41:53 +0800

    Update Films.tsx
    1. Used updated api.ts for API calls.
    2. Specified expected return type from API call.
    3. Added padding to component.

--- src/pages/Films.tsx
+++ src/pages/Films.tsx
@@ -1,19 +1,15 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import api from "../api";
 import Card from "../components/Card";
+import { Film } from "../model/Film";
 
 function Films() {
-  const [films, setFilms] = useState([]);
+  const [films, setFilms] = useState<Film[]>([]);
   useEffect(() => {
-    axios
-      .get("https://tiger-allowing-presumably.ngrok-free.app/api/v1/films", {
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-        },
-      })
+    api
+      .get("/films")
       .then((response) => {
         setFilms(response.data);
-        console.log(response.data);
       })
       .catch((error) => {
         console.error(error);
@@ -22,10 +18,13 @@ function Films() {
 
   return (
     <div className="container pt-5">
+      <div className="browse-by py-3">
+        <h3>Browsing By Films</h3>
+      </div>
       <div className="row g-5">
         {films.map((item) => (
-          <div className="col-3">
-            <Card popover={true} film={item}></Card>
+          <div key={item.id} className="col-3">
+            <Card film={item}></Card>
           </div>
         ))}
       </div>
