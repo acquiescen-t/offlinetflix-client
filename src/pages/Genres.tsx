e50429c186a2b8568c1732b363d487c6035a8a75
Trevor <trevorchew.kh@gmail.com>
2024-11-18 13:44:03 +0800

    Update Genres.tsx
    1. Used updated api.ts for API calls.
    2. Added padding to component.
    3. Maps genres to be displayed based on Genres retrieved from API call.

--- src/pages/Genres.tsx
+++ src/pages/Genres.tsx
@@ -1,98 +1,34 @@
+import { useState, useEffect } from "react";
+import api from "../api";
 import Card from "../components/Card";
 import { Genre } from "../model/Genre";
 
 const Genres = () => {
-  var genres = Genre.GenerateGenres();
+  const [genres, setGenres] = useState<Genre[]>([]);
+  useEffect(() => {
+    api
+      .get("/genres")
+      .then((response) => {
+        setGenres(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
       <div className="container pt-5">
         <div className="row g-5">
-          <div className="col-3">
-            <Card popover={false} genre={genres[0]}></Card>
-          </div>
-          <div className="col-3">
-            <Card popover={false} genre={genres[1]}></Card>
-          </div>
-          <div className="col-3">
-            <Card popover={false} genre={genres[2]}></Card>
-          </div>
-          <div className="col-3">
-            <Card popover={false} genre={genres[3]}></Card>
-          </div>
-          <div className="col-3">
-            <Card popover={false} genre={genres[4]}></Card>
-          </div>
+          {genres.map((item) => (
+            <div key={item.id} className="col-3">
+              <Card genre={item}></Card>
+            </div>
+          ))}
         </div>
       </div>
     </>
   );
 };
-/*
-const Genres2 = () => {
-  return (
-    <>
-      <div className="container pt-5">
-        <div className="row g-5">
-          <div className="col-3">
-            <Card
-              popover={false}
-              posterUrl={horror}
-              title="Horror"
-              year={0}
-              genreList={[]}
-              starList={[]}
-              synopsis=""
-            ></Card>
-          </div>
-          <div className="col-3">
-            <Card
-              popover={false}
-              posterUrl={scifi}
-              title="Science Fiction"
-              year={0}
-              genreList={[]}
-              starList={[]}
-              synopsis=""
-            ></Card>
-          </div>
-          <div className="col-3">
-            <Card
-              popover={false}
-              posterUrl={comedy}
-              title="Comedy"
-              year={0}
-              genreList={[]}
-              starList={[]}
-              synopsis=""
-            ></Card>
-          </div>
-          <div className="col-3">
-            <Card
-              popover={false}
-              posterUrl={thriller}
-              title="Thriller"
-              year={0}
-              genreList={[]}
-              starList={[]}
-              synopsis=""
-            ></Card>
-          </div>
-          <div className="col-3">
-            <Card
-              popover={false}
-              posterUrl={fantasy}
-              title="Fantasy"
-              year={0}
-              genreList={[]}
-              starList={[]}
-              synopsis=""
-            ></Card>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-*/
+
 export default Genres;
