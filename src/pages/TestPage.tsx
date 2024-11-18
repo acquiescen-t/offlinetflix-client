af162e076fa5ded66ac299fb6d901a090736c640
Trevor <trevorchew.kh@gmail.com>
2024-11-18 13:43:19 +0800

    Create TestPage.tsx

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div className="test-container">
+      <span className="badge rounded-pill text-bg-primary">Horror</span>
+      <span className="badge rounded-pill text-bg-primary">Thriller</span>
+      <span className="badge rounded-pill text-bg-primary">Crime</span>
+    </div>
+  );
+};
+
+export default TestPage;
