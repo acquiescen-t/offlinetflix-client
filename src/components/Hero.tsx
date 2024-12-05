cdc3c28e7a886bae268d76664b9f904e8b906935
Trevor <trevorchew.kh@gmail.com>
2024-12-05 16:51:02 +0800

    Update Hero.tsx
    1. Updated reference to InternalApi
    2. Temporarily feature films with the most genres to test visual layout
    3. Pause carousel on hover of description at the side
    4. Sort genres alphabetically

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Film } from "../model/Film";
-import api from "../api";
+import api from "../InternalApi";
 import errorImg from "../assets/web/poster-not-found.jpg";
 import { Link } from "react-router-dom";
 import config from "../Config";
 
+declare var bootstrap: any;
+
 const Hero = () => {
   const [activeSlide, setActiveSlide] = useState(0);
   const [featuredFilms, setFeaturedFilms] = useState<Film[]>([]);
+  const carouselRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     api
-      .get("/films/get-random/4")
+      //.get("/films/get-random/4")
+      .get("/films/get-by-most-genres/4")
       .then((response) => {
         setFeaturedFilms(response.data);
       })
@@ -21,7 +25,7 @@ const Hero = () => {
   }, []);
 
   useEffect(() => {
-    const carouselElement = document.getElementById("heroCarousel");
+    const carouselElement = carouselRef.current;
     const handleSlideChange = (event: any) => {
       const newIndex = parseInt(event.to, 10);
       setActiveSlide(newIndex);
@@ -41,13 +45,35 @@ const Hero = () => {
     };
   }, []);
 
+  const handleMouseEnter = () => {
+    if (carouselRef.current) {
+      const carouselInstance = bootstrap.Carousel.getInstance(
+        carouselRef.current
+      );
+      if (carouselInstance) {
+        carouselInstance.pause();
+      }
+    }
+  };
+  const handleMouseLeave = () => {
+    if (carouselRef.current) {
+      const carouselInstance = bootstrap.Carousel.getInstance(
+        carouselRef.current
+      );
+      if (carouselInstance) {
+        carouselInstance.cycle();
+      }
+    }
+  };
+
   return (
-    <div id="hero" className="hero row">
+    <div id="hero" className="hero px-5 row g-5">
       <div
         id="heroCarousel"
-        className="carousel slide carousel-fade col-8"
+        className="carousel slide carousel-fade col-9"
         data-bs-ride="carousel"
         data-bs-pause="hover"
+        ref={carouselRef}
       >
         <div className="carousel-inner">
           {featuredFilms.map((film, index) => (
@@ -70,92 +96,124 @@ const Hero = () => {
               </div>
             </div>
           ))}
-        </div>
 
-        <div className="carousel-indicators">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="3"
+              aria-label="Slide 4"
+            />
+          </div>
           <button
+            className="carousel-control-prev"
             type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          />
+            data-bs-target="#heroCarousel"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
           <button
+            className="carousel-control-next"
             type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          />
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          />
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="3"
-            aria-label="Slide 4"
-          />
+            data-bs-target="#heroCarousel"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#heroCarousel"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#heroCarousel"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
-      <div className="carousel-description col-3 ms-5">
-        <Link
-          className="carousel-film-name-link"
-          to={`${config.webUrl}/films/${featuredFilms[activeSlide]?.id}`}
-        >
-          {featuredFilms[activeSlide]?.name}
-        </Link>{" "}
-        <br /> <br /> <p>{featuredFilms[activeSlide]?.synopsis}</p> <br />{" "}
-        <div className="carousel-film-lists row pt-3">
+      <div
+        className="carousel-description col-3"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="carousel-film-name-link py-2">
+          <Link to={`${config.webUrl}/films/${featuredFilms[activeSlide]?.id}`}>
+            {featuredFilms[activeSlide]?.name}
+          </Link>
+        </div>
+        <div className="carousel-film-synopsis">
+          <p>{featuredFilms[activeSlide]?.synopsis}</p>
+        </div>
+        <div className="carousel-film-lists row g-1">
           <div className="carousel-film-genres col-6">
-            <h3>GENRES</h3>
-            <ul>
-              {featuredFilms[activeSlide]?.genresOfFilm.map((genre) => (
-                <li key={genre.id}>
-                  <Link to={`${config.webUrl}/genres/${genre.name}`}>
-                    {genre.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <div className="genre-header">
+              <h3>GENRES</h3>
+            </div>
+            <div className="genre-list">
+              <ul>
+                {featuredFilms[activeSlide]?.genresOfFilm
+                  .sort((g1, g2) => {
+                    if (g1.name > g2.name) {
+                      return 1;
+                    }
+                    if (g1.name < g2.name) {
+                      return -1;
+                    }
+
+                    return 0;
+                  })
+                  .map((genre) => (
+                    <li key={genre.id}>
+                      <a
+                        href={"/genres/".concat(genre.name)}
+                        className="badge text-bg-primary"
+                      >
+                        {genre.name}
+                      </a>
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
           <div className="carousel-film-stars col-6">
-            <ul>
-              {featuredFilms[activeSlide]?.starsOfFilm.map((star) => (
-                <li>
-                  <Link to={`${config.webUrl}stars/${star.name}`}>
-                    {star.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <div className="star-header">
+              <h3>STARS</h3>
+            </div>
+            <div className="invisible-scrollbar star-list">
+              <ul>
+                {featuredFilms[activeSlide]?.starsOfFilm.map((star) => (
+                  <li key={star.id}>
+                    <Link
+                      to={`/stars/${star.name
+                        .replaceAll(" ", "-")
+                        .toLowerCase()}`}
+                    >
+                      {star.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
