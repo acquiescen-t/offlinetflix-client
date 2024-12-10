8735280d4805ebf3c20ce19c897d9bc6da7ca2cf
Trevor <trevorchew.kh@gmail.com>
2024-12-10 18:04:22 +0800

    Update Sync.tsx
    1. Page now correctly calls API sequentially to retrieve and update information about current film.
    2. Added message to display sync status.
    3. Redirects user to previous page upon sync completion.

--- src/pages/Sync.tsx
+++ src/pages/Sync.tsx
@@ -1,69 +1,59 @@
 import { useEffect, useState } from "react";
-import { Film } from "../model/Film";
+import { useNavigate, useLocation } from "react-router-dom";
 import api from "../InternalApi";
 import extApi from "../ExternalApi";
-import config from "../Config";
-import { useLocation, redirect } from "react-router-dom";
+import { Film } from "../model/Film";
 import { TMDBFilm } from "../model/TMDBFilm";
+import { TMDBCredits } from "../model/TMDBCredits";
 
 const Sync = () => {
-  // 1. Fetch current Film
   const location = useLocation();
   const { hash, pathname, search } = location;
   var filmId = pathname.replaceAll("/films/", "").replaceAll("/sync", "");
   console.log(`filmId: ${filmId}`);
 
-  const [currentFilm, setCurrentFilm] = useState<Film>();
+  const [completed, setCompleted] = useState<Boolean>(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (completed) navigate(-1);
+  }, [completed, navigate]);
   useEffect(() => {
     api
       .get(`/films/id/${filmId}`)
-      .then((response) => setCurrentFilm(response.data))
+      .then((response) => {
+        const currentFilm: Film = response.data;
+        let filmName = currentFilm.name.replaceAll(" ", "%20");
+        let releaseYear = currentFilm.releaseYear;
+        let url = `/3/search/movie?query=${filmName}&include_adult=true&page=1&year=${releaseYear}&language=en-US`;
+        return extApi.get(url);
+      })
+      .then((resultsResponse) => {
+        const tmdbFilm: TMDBFilm = resultsResponse.data.results[0];
+        let url = `films/update-film/${filmId}`;
+        return api.put(url, tmdbFilm);
+      })
+      .then((intResponseGenres) => {
+        const updatedFilm: Film = intResponseGenres.data;
+        let url = `/3/movie/${updatedFilm.tmdb_id}/credits?language=en-US`;
+        return extApi.get(url);
+      })
+      .then((creditsResponse) => {
+        const filmCredits: TMDBCredits = creditsResponse.data;
+        let url = `films/${filmId}/update-stars`;
+        return api.put(url, filmCredits);
+      })
+      .then((intResponseStars) => {
+        const updatedStars: Film = intResponseStars.data;
+        setCompleted(true);
+      })
       .catch((error) => console.error(error));
   }, []);
 
-  //2. Call external API for information about current Film
-  const [tmdbResponse, setTmdbResponse] = useState<TMDBFilm>();
-  useEffect(() => {
-    let url = `/3/search/movie?query=${currentFilm?.name.replaceAll(
-      " ",
-      "%20"
-    )}&include_adult=true&page=1&year=${
-      currentFilm?.releaseYear
-    }&language=en-US`;
-
-    extApi
-      .get(url)
-      .then((response) => {
-        setTmdbResponse(response.data.results[0]);
-        console.log(tmdbResponse);
-      })
-      .catch((err) => console.error(err));
-  }, [currentFilm]);
-
-  // 3. Update current Film based on information retrieved
-  const [updatedFilm, setUpdatedFilm] = useState<Film>();
-  useEffect(() => {
-    api
-      .put(`films/update-film/${filmId}`, tmdbResponse)
-      .then((response) => setUpdatedFilm(response.data))
-      .catch((err) => console.error(err));
-  }, [tmdbResponse]);
-
   return (
     <div className="sync-page">
-      <div className="sync-page-film-title">
-        <h1>FILM NAME</h1>
-        <p>
-          {updatedFilm?.name} ({updatedFilm?.releaseYear})
-        </p>
-      </div>
-      <div className="sync-page-film-genres">
-        <h1>FILM GENRES</h1>
-        {updatedFilm?.genresOfFilm.map((item) => (
-          <div key={item.id} className="col-3">
-            <p>{item.name}</p>
-          </div>
-        ))}
+      <div className="container">
+        <p>{completed ? "Completed!" : "Syncing..."}</p>
       </div>
     </div>
   );
